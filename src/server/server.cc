846ade20ed42b89f13f0b501bab37a6d51d60b1b
axzz <13408058303@163.com>
2020-05-26 21:37:33 +0800

    basic network

---
+++ src/server/server.cc
@@ -0,0 +1,53 @@
+#include "server.h"
+#include <iostream>
+#include <string.h>
+namespace mynet
+{
+  Server::Server(uint16_t port):readCallback(NULL)
+  {
+    if ((listenFd = ::socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+      std::cout << "socket error" << std::endl;
+      exit(1);
+    }
+
+    bzero(&address, sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_port = htons(port);
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (::bind(listenFd, (sockaddr *)&address, sizeof(address)) < 0)
+    {
+      std::cout << "bind error" << std::endl;
+      exit(1);
+    }
+  }
+
+  void Server::setReadEventCallback(ReadEventCallback cb){
+      readCallback=cb;
+  }
+
+  void Server::start()
+  {
+    startListen();
+    while (true)
+    {
+      Poller::instance().poll(activeSockets);
+      for (auto activeSocket : activeSockets)
+      {
+        activeSocket->handleEvents();
+        // 可以直接让其在poll中干
+      }
+    }
+  }
+
+
+
+  void Server::startListen()
+  {
+    std::cout << "start listenning" << std::endl; 
+    Socket *accepter = new Socket(listenFd);
+    accepter->setReadCallback(std::bind(&Socket::connect, accepter, _1, readCallback));
+    ::listen(accepter->fd(), 100);
+  }
+} // namespace rudis
---
+++ src/server/socket.cc
@@ -0,0 +1,118 @@
+#include "server.h"
+#include <unistd.h>
+#include <iostream>
+
+namespace mynet
+{
+    Socket::Socket(int fd) : fd_(fd), pfd_id(-1)
+    {
+        setReadCallback(std::bind(&Socket::default_read, this, _1));
+        Poller::instance().update(this);
+        std::cout << "inserted to poll" << std::endl;
+        //TODO: log
+    }
+
+    Socket::~Socket()
+    {
+        ::close(fd_);
+        Poller::instance().remove(this);
+    }
+
+    void Socket::handleEvents()
+    {
+        std::cout << revents_ << std::endl;
+        if ((revents_ & POLLHUP) and !(revents_ & POLLIN))
+        {
+            onClose();
+        }
+        if (revents_ & (POLLERR | POLLNVAL))
+        {
+            onError();
+        }
+        if (revents_ & (POLLIN | POLLPRI))
+        {
+            readCallback(*this);
+        }
+    }
+
+    void Socket::connect(Socket &socket, ReadEventCallback cb)
+    {
+        int connfd = ::accept(fd_, NULL, NULL);
+        Socket *connect_socket = new Socket(connfd);
+        if (cb == NULL)
+        {
+            connect_socket->setReadCallback(std::bind(&Socket::default_read, connect_socket, _1));
+        }
+        else
+        {
+            connect_socket->setReadCallback(cb);
+        }
+
+        std::cout << "get new connection" << std::endl;
+    };
+
+    void Socket::close()
+    {
+        std::cout << "Socket closed" << std::endl;
+        delete this;
+    }
+
+    void Socket::shutdown()
+    {
+        close();
+    }
+
+    void Socket::onClose()
+    {
+        close();
+    }
+
+    void Socket::write(string buffer)
+    {
+        const char *ptr = buffer.c_str();
+        size_t left = buffer.size();
+        ssize_t written;
+        while (left > 0)
+        {
+            if ((written = ::write(fd_, ptr, left)) < 0)
+            {
+                if (written < 0 && errno == EINTR)
+                {
+                    written = 0;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            left -= written;
+            ptr += written;
+        }
+    }
+
+    void Socket::write(char *buffer, int n)
+    {
+        string b(buffer, n);
+        write(b);
+    }
+
+    void Socket::default_read(Socket &socket)
+    {
+        char buf[1024];
+        ssize_t n = ::read(fd_, buf, 1024);
+        if (n == 0)
+        {
+            close();
+        }
+        else
+        {
+            write(buf, n);
+        }
+    }
+
+    void Socket::onError()
+    {
+        close();
+    }
+
+} // namespace mynet
