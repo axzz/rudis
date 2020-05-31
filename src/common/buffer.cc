dff8715d793fbd43cc57fcaac0ed64595982442e
axzz <13408058303@163.com>
2020-06-01 04:28:22 +0800

    reply buffer

--- src/common/buffer.cc
+++ src/common/buffer.cc
@@ -2,10 +2,12 @@
 #include "unistd.h"
 
 #include <iostream>
+
+//TODO: 考虑客户端不规范问题
 namespace rudis
 {
-
-    //TODO: 考虑客户端不规范问题
+    static const char CRLF[] = "\r\n";
+    // TODO: 变成验证char
     inline void drop_char(Socket &skt)
     {
         char v;
@@ -14,7 +16,7 @@ namespace rudis
 
     int get_number(Socket &skt)
     {
-        char var='0';
+        char var = '0';
         int len = 0;
         do
         {
@@ -31,14 +33,14 @@ namespace rudis
         int len = get_number(skt);
         RString res(len, ' ');
         ::read(skt.fd(), &res[0], len);
-        drop_char(skt);drop_char(skt);
+        drop_char(skt);
+        drop_char(skt);
         return res;
     }
 
     vector<RString> reveive_buffer(Socket &skt)
     {
         // 获得命令数
-        char var[32];
         drop_char(skt);
         int len = get_number(skt);
         vector<RString> res;
@@ -49,10 +51,89 @@ namespace rudis
         return res;
     }
 
-    void callback_test(Socket &skt){
+    void callback_test(Socket &skt)
+    {
         auto res = reveive_buffer(skt);
-        for(auto& str:res){
+        for (auto &str : res)
+        {
             std::cout << str << std::endl;
         }
+    }
+
+    void ReplyBuffer::clear()
+    {
+        buffer.clear();
+    }
+
+    void ReplyBuffer::push(RString str)
+    {
+        for (char c : str)
+        {
+            buffer.push_back(c);
+        }
+    }
+
+    void ReplyBuffer::push_CRLF()
+    {
+        for (char c : CRLF)
+        {
+            buffer.push_back(c);
+        }
+    }
+
+    void ReplyBuffer::reply_ok()
+    {
+        reply_state("OK");
+    }
+
+    void ReplyBuffer::reply_state(RString str)
+    {
+        clear();
+        push("+");
+        push(str);
+        push_CRLF();
+    }
+
+    void ReplyBuffer::reply_simple_error()
+    {
+        reply_error("err");
+    }
+
+    void ReplyBuffer::reply_error(RString str)
+    {
+        clear();
+        push("-");
+        push(str);
+        push_CRLF();
+    }
+
+    void ReplyBuffer::push_int(int num)
+    {
+        push(":");
+        push(std::to_string(num));
+        push_CRLF();
+    }
+
+    void ReplyBuffer::push_str(RString str)
+    {
+        if (str.size() == 0)
+        {
+            push("$-1");
+            push_CRLF();
+        }
+        else
+        {
+            push("$" + std::to_string(str.size()));
+            push_CRLF();
+            push(str);
+            push_CRLF();
+        }
+    }
+
+    void ReplyBuffer::mutli_init(int num)
+    {
+        clear();
+        push("*" + std::to_string(num));
+        push_CRLF();
     }
 } // namespace rudis
