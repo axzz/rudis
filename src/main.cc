741d8a56bb56e98e05c898f63291621a6da1ac82
axzz <13408058303@163.com>
2020-06-02 00:00:06 +0800

    simple server

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,23 @@
 #include <iostream>
+#include "server.h"
+#include "object.h"
+#include "store.h"
+#include "buffer.h"
+#include "command.h"
+using namespace rudis;
+using namespace std;
 
-int main(){
-    std::cout<<"hello world"<<std::endl;
+void callback_test(Socket &skt)
+{
+    vector<RString> params = reveive_buffer(skt);
+    ReplyBuffer rpy;
+    COMMAND.execute_command(params, rpy);
+    skt.write(rpy.c_str(), rpy.size());
+}
+
+int main()
+{
+    Server server(6324);
+    server.setReadEventCallback(callback_test);
+    server.start();
 }
